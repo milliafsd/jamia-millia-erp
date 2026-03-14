84739803c5caa2b0c5eae48211156e0325f001dd
milliafsd <milliafsd1@gmail.com>
2026-03-15 00:03:43 +0500

    Implement database connection and initialization
    Added functions to connect to the database and initialize tables for students and teachers.

--- database.py
+++ database.py
@@ -1,1 +1,37 @@
+import sqlite3
 
+DB="jamia.db"
+
+def connect():
+
+    conn = sqlite3.connect(DB, check_same_thread=False)
+
+    return conn
+
+
+def init_db():
+
+    conn=connect()
+
+    c=conn.cursor()
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS students(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    father TEXT,
+    teacher TEXT
+    )
+    """)
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS teachers(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    password TEXT
+    )
+    """)
+
+    conn.commit()
+
+    return conn
