29e258c56f2a9d74415160d694ad2376b0a6bb92
milliafsd <milliafsd1@gmail.com>
2026-03-15 00:04:19 +0500

    Implement login functionality with Streamlit

--- auth.py
+++ auth.py
@@ -1,1 +1,32 @@
+import streamlit as st
+from database import connect
 
+def login():
+
+    conn=connect()
+
+    c=conn.cursor()
+
+    st.subheader("لاگ ان")
+
+    user=st.text_input("Username")
+
+    pwd=st.text_input("Password",type="password")
+
+    if st.button("لاگ ان"):
+
+        r=c.execute(
+        "SELECT * FROM teachers WHERE name=? AND password=?",
+        (user,pwd)
+        ).fetchone()
+
+        if r:
+
+            st.session_state["login"]=True
+            st.session_state["user"]=user
+
+            st.rerun()
+
+        else:
+
+            st.error("غلط معلومات")
